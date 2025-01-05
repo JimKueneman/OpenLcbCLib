7dd180d53319def9e42910d9d77ef4d8246ef4d9
Jim Kueneman <JimKueneman@yahoo.com>
2025-01-05 12:26:30 -0700

    added the driver files for TurnoutBOSS

--- src/applications/dspic/TurnoutBOSS.X/turnoutboss_drivers.h
+++ src/applications/dspic/TurnoutBOSS.X/turnoutboss_drivers.h
@@ -25,9 +25,8 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- * \file io_pinout.h
+ * \file dsPIC33EPxxxGP50x_drivers.h
  *
- *  Assigns the IO to the dspic33EP512GP504
  *
  * @author Jim Kueneman
  * @date 3 Jan 2025
@@ -35,10 +34,27 @@
 
 // This is a guard condition so that contents of this file are not included
 // more than once.  
-#ifndef __IO_PINOUT__
-#define	__IO_PINOUT__
+#ifndef __TURNOUTBOSS_DRIVERS__
+#define	__TURNOUTBOSS_DRIVERS__
 
 #include <xc.h> // include processor files - each processor file is guarded.  
+
+#include "../../../openlcb/openlcb_types.h"
+
+// UART ------------------------------------------------------------------------
+// with FCY = 40000000UL
+
+#define FP FCY
+#define BAUDRATE 333333
+#define BRGVAL_BRGH_L ((FP/BAUDRATE)/16)-1
+#define BRGVAL_BRGH_H ((FP/BAUDRATE)/4)-1
+
+#define BRG_OFFSET 0
+
+// Oscillator ------------------------------------------------------------------
+// Fine tune to get exactly 40Mhz
+
+#define PLLDIV_OFFSET -2
 
 
 #define TRACK_DETECT_GAIN_1_CS _RB14
@@ -103,11 +119,28 @@
 #define LED _RB9
 #define LED_TRIS _TRISB9
 
+// Assign the function pointer to where the UART Rx should call back with the byte it received
+// WARNING: Is in the context of the interrupt, be careful
+// void func(rx_data);
+typedef void (*uart_rx_callback_t) (uint16_olcb_t);
+
 #ifdef	__cplusplus
 extern "C" {
 #endif /* __cplusplus */
 
-    extern void IO_PINOUT_intialize(void);
+    extern void TurnoutBossDrivers_setup(parameterless_callback_t _100ms_timer_sink);
 
-#endif	/* __IO_PINOUT__ */
+    extern void TurnoutBossDrivers_reboot(void);
+
+    extern void TurnoutBossDrivers_assign_uart_rx_callback(uart_rx_callback_t uart_rx_callback);
+    
+    extern uint16_olcb_t TurnoutBossDrivers_config_mem_read(uint32_olcb_t address, uint16_olcb_t count, configuration_memory_buffer_t* buffer);
+    
+    extern uint16_olcb_t TurnoutBossDrivers_config_mem_write(uint32_olcb_t address, uint16_olcb_t count, configuration_memory_buffer_t* buffer);
+    
+    extern void TurnoutBossDrivers_pause_100ms_timer();
+    
+    extern void TurnoutBossDrivers_resume_100ms_timer();
+
+#endif	/* __TURNOUTBOSS_DRIVERS__ */
 
