20398fb30adafb02132ef3bbcad5c1968b734a3f
Jim Kueneman <JimKueneman@yahoo.com>
2025-03-06 15:11:03 -0700

    - Updated the linker files to be 4 bytes short of the end of an erase page since I won't allow odd number of instructions to be written (since you must write 2 instructions/4 bytes at a time) - Updated the build script to experiment with -fill and -checksum

--- src/applications/dspic/TurnoutBossCommon/turnoutboss_build.sh
+++ src/applications/dspic/TurnoutBossCommon/turnoutboss_build.sh
@@ -6,8 +6,14 @@ pwd
 
 echo "Generating the Merged Hex File (currently not useful as it does not have the CheckSum embedded in it)"
 
-"/Applications/microchip/mplabx/v6.20/mplab_platform/bin/hexmate" r0xB000-0x55800,"../TurnoutBOSS.X/dist/default/production/TurnoutBOSS.X.production.hex" r0x0000-0xB000,"../TurnoutBossBootloader.X/dist/default/production/TurnoutBossBootloader.X.production.hex" -addressing=2 -o../turnoutboss_merged.hex 
+echo "Generating a Big Endian Merged Version (probably the correct way to do it)"
 
+"/Applications/microchip/mplabx/v6.20/mplab_platform/bin/hexmate" r0xB000-0x55800,"../TurnoutBOSS.X/dist/default/production/TurnoutBOSS.X.production.hex" r0x0000-0xB000,"../TurnoutBossBootloader.X/dist/default/production/TurnoutBossBootloader.X.production.hex" -fill=w1:0xAA@0xB000:0x53FFC -ck=0xB000-0x53FFC@0x54800w3g3 -addressing=2 -o../turnoutboss_merged_big_endian.hex 
+
+
+echo "Generating a Little Endian Merged Version"
+
+"/Applications/microchip/mplabx/v6.20/mplab_platform/bin/hexmate" r0xB000-0x55800,"../TurnoutBOSS.X/dist/default/production/TurnoutBOSS.X.production.hex" r0x0000-0xB000,"../TurnoutBossBootloader.X/dist/default/production/TurnoutBossBootloader.X.production.hex" -fill=w1:0xAA@0xB000:0x53FFC -ck=0xB000-0x53FFC@0x54800w-3g3 -addressing=2 -o../turnoutboss_merged_little_endian.hex 
 
 
 
