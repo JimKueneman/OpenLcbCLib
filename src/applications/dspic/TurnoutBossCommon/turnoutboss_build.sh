bfaad51208c1885fc5c0f82f337c4040cc54a174
Bob Jacobsen <jacobsen@mac.com>
2025-03-08 12:24:47 -0500

    use addressing=2

--- src/applications/dspic/TurnoutBossCommon/turnoutboss_build.sh
+++ src/applications/dspic/TurnoutBossCommon/turnoutboss_build.sh
@@ -1,6 +1,6 @@
 
 echo "Converting the hex file to a dshex file..."
-
+t
 ../TurnoutBossCommon/Hex2dsHex/hex2dshex -f ../TurnoutBOSS.X/dist/default/production/TurnoutBOSS.X.production.hex -m 0xB000 -x 0x55000 -e 1024 -c 0x54800 -k MustangpeakEngineeringTurnoutBoss2.0
 
 
@@ -15,5 +15,7 @@ echo "Generating the hex file with checksum information"
 # third -fill put 0 in otherwise not loaded memory
 # the -ck inserts a one-byte checksum
 
-"/Applications/microchip/mplabx/v6.20/mplab_platform/bin/hexmate" ../turnoutboss_merged.hex -o../turnoutboss_merged_summed.hex -fill=w1:0x00,0x60,0x00,0x00@0xa9000:0xa9002 -fill=w1:0xFF,0x47,0x05,0x00@0xa9004:0xa9007 -fill=00@0x6000:0x547ff -ck=0C000-0a8fff@a9008w1g1
+# "/Applications/microchip/mplabx/v6.20/mplab_platform/bin/hexmate" ../turnoutboss_merged.hex -o../turnoutboss_merged_summed.hex -fill=w1:0x00,0x60,0x00,0x00@0xa9000:0xa9002 -fill=w1:0xFF,0x47,0x05,0x00@0xa9004:0xa9007 -fill=00@0x6000:0x547ff -ck=0C000-0a8fff@a9008w1g1
+
+"/Applications/microchip/mplabx/v6.20/mplab_platform/bin/hexmate" ../turnoutboss_merged.hex -o../turnoutboss_merged_summed.hex -addressing=2 -fill=w1:0x00,0x60,0x00,0x00@0x54800:0x54801 -fill=w1:0xFF,0x47,0x05,0x00@0x54802:0x54803 -fill=00@0x6000:0x547ff -ck=06000-0547ff@54804w1g1
 
