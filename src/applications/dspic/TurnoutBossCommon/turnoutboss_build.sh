c45dadd2e65555586e987049ef31deaa77e2a31d
Bob Jacobsen <jacobsen@mac.com>
2025-03-09 14:18:46 -0400

    fix memory ranges

--- src/applications/dspic/TurnoutBossCommon/turnoutboss_build.sh
+++ src/applications/dspic/TurnoutBossCommon/turnoutboss_build.sh
@@ -17,5 +17,7 @@ echo "Generating the hex file with checksum information"
 
 # "/Applications/microchip/mplabx/v6.20/mplab_platform/bin/hexmate" ../turnoutboss_merged.hex -o../turnoutboss_merged_summed.hex -fill=w1:0x00,0x60,0x00,0x00@0xa9000:0xa9002 -fill=w1:0xFF,0x47,0x05,0x00@0xa9004:0xa9007 -fill=00@0x6000:0x547ff -ck=0C000-0a8fff@a9008w1g1
 
-"/Applications/microchip/mplabx/v6.20/mplab_platform/bin/hexmate" ../turnoutboss_merged.hex -o../turnoutboss_merged_summed.hex -addressing=2 -fill=w1:0x00,0x60,0x00,0x00@0x54800:0x54801 -fill=w1:0xFF,0x47,0x05,0x00@0x54802:0x54803 -fill=00@0x6000:0x547ff -ck=06000-0547ff@54804w1g1
+# "/Applications/microchip/mplabx/v6.20/mplab_platform/bin/hexmate" ../turnoutboss_merged.hex -o../turnoutboss_merged_summed.hex -addressing=2 -fill=w1:0x00,0x60,0x00,0x00@0x54800:0x54801 -fill=w1:0xFF,0x47,0x05,0x00@0x54802:0x54803 -fill=00@0x6000:0x547ff -ck=06000-0547ff@54804w1g1
+
+"/Applications/microchip/mplabx/v6.20/mplab_platform/bin/hexmate" ../turnoutboss_merged.hex -o../turnoutboss_merged_summed.hex -addressing=2 -fill=w1:0x00,0xB0,0x00,0x00@0x54800:0x54801 -fill=w1:0xFF,0x3F,0x05,0x00@0x54802:0x54803 -fill=00@0xB000:0x53FFF -ck=0B000-0538ff@54804w1g1
 
