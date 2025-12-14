7d86440dbeb6cbc4f62157375023dbc72eae480e
Jim Kueneman <JimKueneman@yahoo.com>
2025-12-14 14:28:01 -0700

    Added some Images to the help file Added a mainpage.h file to contain the doxygen code for the main page

--- src/mainpage.h
+++ src/mainpage.h
@@ -1,12 +1,15 @@
 /** 
  * @mainpage OpenLcbCLib Documentation
  * 
- * @subsection Welcome
  * 
- * OpenLcbCLib is a library to create OpenLcb (or NMRA LCC) nodes in C.  It handles
- * all low level interactions required to be compliant on the OpenLcb/LCC bus and enables
- * the developer to focus on writing application specific code.
- *  
+ * @section Welcome
+ * 
+ * <div style="display: flex; align-items: center;">
+ * <img src="Diesel.png" alt="Image" align="left" style="margin-right: 15px; width: 128px;">
+ *   <span>OpenLcbCLib is a library to create OpenLcb (or NMRA LCC) nodes in C.  It handles all low level interactions required to be compliant on the OpenLcb/LCC bus and enables the developer to focus on writing application specific code.</span>
+ * </div>
+ * 
+ * 
  * 
  * @copyright Copyright (c) 2025, Jim Kueneman
  *
