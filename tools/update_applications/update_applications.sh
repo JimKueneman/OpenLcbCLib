d7e17e88ee28080bb60f923064e4ca2d4ef8847d
Jim Kueneman <JimKueneman@yahoo.com>
2025-12-16 08:45:46 -0700

    Reorganized the Tools Folder

---
+++ tools/update_applications/update_applications.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+cp ../../src/openlcb/*.c ../../src/applications/arduino/EPS32/BasicNode/src/openlcb/
+cp ../../src/openlcb/*.h ../../src/applications/arduino/EPS32/BasicNode/src/openlcb/
+#cp ../../src/drivers/*.c ../../src/applications/arduino/EPS32/BasicNode/src/drivers/
+#cp ../../src/drivers/*.h ../../src/applications/arduino/EPS32/BasicNode/src/drivers/
+cp ../../src/drivers/common/*.c ../../src/applications/arduino/EPS32/BasicNode/src/drivers/common/
+cp ../../src/drivers/common/*.h ../../src/applications/arduino/EPS32/BasicNode/src/drivers/common/
+cp ../../src/utilities/*.c ../../src/applications/arduino/EPS32/BasicNode/src/utilities/
+cp ../../src/utilities/*.h ../../src/applications/arduino/EPS32/BasicNode/src/utilities/
+
+
+
+
+cp ../../src/openlcb/*.c ../../src/applications/platformio/ESP32/BasicNode/src/src/openlcb/
+cp ../../src/openlcb/*.h ../../src/applications/platformio/ESP32/BasicNode/src/src/openlcb/
+#cp ../../src/drivers/*.c ../../src/applications/platformio/ESP32/BasicNode/src/src/drivers/
+#cp ../../src/drivers/*.h ../../src/applications/platformio/ESP32/BasicNode/src/src/drivers/
+cp ../../src/drivers/common/*.c ../../src/applications/platformio/ESP32/BasicNode/src/src/drivers/common/
+cp ../../src/drivers/common/*.h ../../src/applications/platformio/ESP32/BasicNode/src/src/drivers/common/
+cp ../../src/utilities/*.c ../../src/applications/platformio/ESP32/BasicNode/src/src/utilities/
+cp ../../src/utilities/*.h ../../src/applications/platformio/ESP32/BasicNode/src/src/utilities/
+
+cp ../../src/openlcb/*.c ../../src/applications/platformio/ESP32/BasicNode_WiFi/src/src/openlcb/
+cp ../../src/openlcb/*.h ../../src/applications/platformio/ESP32/BasicNode_WiFi/src/src/openlcb/
+#cp ../../src/drivers/*.c ../../src/applications/platformio/ESP32/BasicNode_WiFi/src/src/drivers/
+#cp ../../src/drivers/*.h ../../src/applications/platformio/ESP32/BasicNode_WiFi/src/src/drivers/
+cp ../../src/drivers/common/*.c ../../src/applications/platformio/ESP32/BasicNode_WiFi/src/src/drivers/common/
+cp ../../src/drivers/common/*.h ../../src/applications/platformio/ESP32/BasicNode_WiFi/src/src/drivers/common/
+cp ../../src/utilities/*.c ../../src/applications/platformio/ESP32/BasicNode_WiFi/src/src/utilities/
+cp ../../src/utilities/*.h ../../src/applications/platformio/ESP32/BasicNode_WiFi/src/src/utilities/
+
+cp ../../src/openlcb/*.c ../../src/applications/platformio/OSX/BasicNode/src/src/openlcb/
+cp ../../src/openlcb/*.h ../../src/applications/platformio/OSX/BasicNode/src/src/openlcb/
+#cp ../../src/drivers/*.c ../../src/applications/platformio/OSX/BasicNode/src/src/drivers/
+#cp ../../src/drivers/*.h ../../src/applications/platformio/OSX/BasicNode/src/src/drivers/
+cp ../../src/drivers/common/*.c ../../src/applications/platformio/OSX/BasicNode/src/src/drivers/common/
+cp ../../src/drivers/common/*.h ../../src/applications/platformio/OSX/BasicNode/src/src/drivers/common/
+cp ../../src/utilities/*.c ../../src/applications/platformio/OSX/BasicNode/src/src/utilities/
+cp ../../src/utilities/*.h ../../src/applications/platformio/OSX/BasicNode/src/src/utilities/
+
+
+
+
+cp ../../src/openlcb/*.c ../../src/applications/xcode/BasicNode/src/openlcb/
+cp ../../src/openlcb/*.h ../../src/applications/xcode/BasicNode/src/openlcb/
+#cp ../../src/drivers/*.c ../../src/applications/xcode/BasicNode/src/drivers/
+#cp ../../src/drivers/*.h ../../src/applications/xcode/BasicNode/src/drivers/
+cp ../../src/drivers/common/*.c ../../src/applications/xcode/BasicNode/src/drivers/common/
+cp ../../src/drivers/common/*.h ../../src/applications/xcode/BasicNode/src/drivers/common/
+cp ../../src/utilities/*.c ../../src/applications/xcode/BasicNode/src/utilities/
+cp ../../src/utilities/*.h ../../src/applications/xcode/BasicNode/src/utilities/
+
+
+
+
+cp ../../src/openlcb/*.c ../../src/applications/ti_thiea/MSPM03507_Launchpad/BasicNode/src/openlcb/
+cp ../../src/openlcb/*.h ../../src/applications/ti_thiea/MSPM03507_Launchpad/BasicNode/src/openlcb/
+#cp ../../src/drivers/*.c ../../src/applications/ti_thiea/MSPM03507_Launchpad/BasicNode/src/drivers/
+#cp ../../src/drivers/*.h ../../src/applications/ti_thiea/MSPM03507_Launchpad/BasicNode/src/drivers/
+cp ../../src/drivers/common/*.c ../../src/applications/ti_thiea/MSPM03507_Launchpad/BasicNode/src/drivers/common/
+cp ../../src/drivers/common/*.h ../../src/applications/ti_thiea/MSPM03507_Launchpad/BasicNode/src/drivers/common/
+cp ../../src/utilities/*.c ../../src/applications/ti_thiea/MSPM03507_Launchpad/BasicNode/src/utilities/
+cp ../../src/utilities/*.h ../../src/applications/ti_thiea/MSPM03507_Launchpad/BasicNode/src/utilities/
+
+
+
+cp ../../src/openlcb/*.c ../../src/applications/stm32_cubeide/STM32F407-Discovery/BasicNode/Core/Src/src/openlcb/
+cp ../../src/openlcb/*.h ../../src/applications/stm32_cubeide/STM32F407-Discovery/BasicNode/Core/Src/src/openlcb/
+#cp ../../src/drivers/*.c ../../src/applications/stm32_cubeide/STM32F407-Discovery/BasicNode/Core/Src/src/drivers/
+#cp ../../src/drivers/*.h ../../src/applications/stm32_cubeide/STM32F407-Discovery/BasicNode/Core/Src/src/drivers/
+cp ../../src/drivers/common/*.c ../../src/applications/stm32_cubeide/STM32F407-Discovery/BasicNode/Core/Src/src/drivers/common/
+cp ../../src/drivers/common/*.h ../../src/applications/stm32_cubeide/STM32F407-Discovery/BasicNode/Core/Src/src/drivers/common/
+cp ../../src/utilities/*.c ../../src/applications/stm32_cubeide/STM32F407-Discovery/BasicNode/Core/Src/src/utilities/
+cp ../../src/utilities/*.h ../../src/applications/stm32_cubeide/STM32F407-Discovery/BasicNode/Core/Src/src/utilities/
