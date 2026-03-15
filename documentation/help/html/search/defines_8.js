fae9456f21977c13017419739799dcbe763ad372
Jim Kueneman <JimKueneman@yahoo.com>
2026-03-15 15:43:55 -0700

    forgot to run doxygen on v1.0

--- documentation/help/html/search/defines_8.js
+++ documentation/help/html/search/defines_8.js
@@ -5,5 +5,9 @@ var searchData=
   ['offset_5fdest_5fid_5fin_5fidentifier_2',['OFFSET_DEST_ID_IN_IDENTIFIER',['../can__rx__statemachine_8c.html#ae7f41e4faab5df648f046b765d88b752',1,'can_rx_statemachine.c']]],
   ['offset_5fdest_5fid_5fin_5fpayload_3',['OFFSET_DEST_ID_IN_PAYLOAD',['../can__rx__statemachine_8c.html#a59d0bf671bc6a176905d3e5a9b9ce25e',1,'can_rx_statemachine.c']]],
   ['offset_5fno_5fdest_5fid_4',['OFFSET_NO_DEST_ID',['../can__rx__statemachine_8c.html#ac1b43935138fae3071934e63934934cc',1,'can_rx_statemachine.c']]],
-  ['openlcb_5fevent_5fid_5foffset_5',['OPENLCB_EVENT_ID_OFFSET',['../openlcb__node_8c.html#ac2a7e31805a9b2f6cc448d65a06935f1',1,'openlcb_node.c']]]
+  ['openlcb_5fc_5flib_5fversion_5',['OPENLCB_C_LIB_VERSION',['../openlcb__defines_8h.html#a6f45bead013564d24b68c618993d3b38',1,'openlcb_defines.h']]],
+  ['openlcb_5fc_5flib_5fversion_5fmajor_6',['OPENLCB_C_LIB_VERSION_MAJOR',['../openlcb__defines_8h.html#adc08095c3332c10131756272728157dc',1,'openlcb_defines.h']]],
+  ['openlcb_5fc_5flib_5fversion_5fminor_7',['OPENLCB_C_LIB_VERSION_MINOR',['../openlcb__defines_8h.html#a2eb10d1e63a0f5f2cb2d471754893c44',1,'openlcb_defines.h']]],
+  ['openlcb_5fc_5flib_5fversion_5fpatch_8',['OPENLCB_C_LIB_VERSION_PATCH',['../openlcb__defines_8h.html#ac58ddd7b83b9465e59d929cfb07fd847',1,'openlcb_defines.h']]],
+  ['openlcb_5fevent_5fid_5foffset_9',['OPENLCB_EVENT_ID_OFFSET',['../openlcb__node_8c.html#ac2a7e31805a9b2f6cc448d65a06935f1',1,'openlcb_node.c']]]
 ];
